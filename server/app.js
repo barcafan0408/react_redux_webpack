b45f1ecc8f565a4a5dc802f8a150abe4aa998ac2
Alex Bezpalko <a.bespalko88@gmail.com>
2018-09-06 19:37:55 +0300

    deploy

--- server/app.js
+++ server/app.js
@@ -7,9 +7,9 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, '../build')));
 
-//app.get('/', (req, res) => {
-//  res.sendFile(path.resolve(__dirname, '../src/index.html'));
-//});
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../src/index.html'));
+});
 
 const server = app.listen(PORT, () => {
     console.log(`Server is up and running on port ${PORT}`);
